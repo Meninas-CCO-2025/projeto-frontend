ab6a0d294cf277513706a43c070091c1c8c9a4f3
Laís Câmara <laiscamara2007@gmail.com>
2026-05-14 18:47:44 -0300

    Configuração inicial do Cypress e primeiros testes iniciais E2E

--- cypress/e2e/fluxos.cy.js
+++ cypress/e2e/fluxos.cy.js
@@ -2,7 +2,15 @@ describe('Fluxo do site', () => {
 
   it('Acessa a página inicial', () => {
 
-    cy.visit('index.html')
+    cy.visit('http://127.0.0.1:8080')
+
+  })
+
+  it('Verifica se a página carregou', () => {
+
+    cy.visit('http://127.0.0.1:8080')
+
+    cy.get('body').should('exist')
 
   })
 
