f9c613d30f79b5f617f21bdc00c6c93759b7dbcc
Laís Câmara <laiscamara2007@gmail.com>
2026-05-14 19:44:35 -0300

    Implementação de teste de Formulário e Login no Cypress

--- cypress/e2e/fluxos.cy.js
+++ cypress/e2e/fluxos.cy.js
@@ -1,5 +1,5 @@
 /* 
-1. Teste de carregamento da página (acessa o site, verifica se a página abre, valida o body existente).
+1. Teste de carregamento da página 
 2. Teste de navegação 
 3. Teste de formulário/login
 4. Teste de elementos visíveis
@@ -7,16 +7,16 @@
 6. Verificação de texto
 */
 
-
 describe('Fluxo do site', () => {
 
+  // 1. TESTE DE CARREGAMENTO DA PÁGINA
   it('Acessa a página inicial', () => {
 
     cy.visit('http://127.0.0.1:8080')
 
   })
 
-  it('Verifica se a página carregou', () => { // ETAPA 1
+  it('Verifica se a página carregou corretamente', () => {
 
     cy.visit('http://127.0.0.1:8080')
 
@@ -24,21 +24,44 @@ describe('Fluxo do site', () => {
 
   })
 
-  it('Verifica se o menu existe', () => { // ETAPA 2
+  // 2. TESTE DE NAVEGAÇÃO
+  it('Acessa aba de cadastro', () => {
 
-    cy.visit('http://127.0.0.1:8080')
+    cy.visit('http://127.0.0.1:8080/login.html')
 
-    cy.get('nav').should('exist')
+    cy.contains('Cadastrar').click()
 
   })
 
-  it('Clica em um card de música', () => {
+  // 3. TESTE DE FORMULÁRIO / LOGIN
+  it('Preenche formulário de cadastro', () => {
 
-    cy.visit('http://127.0.0.1:8080')
+    cy.visit('http://127.0.0.1:8080/login.html')
 
-    cy.get('.music-card')
-      .first()
-      .click()
+    cy.contains('Cadastrar').click()
+
+    cy.get('#cad-nome')
+      .type('lais')
+
+    cy.get('#cad-email')
+      .type('teste@email.com')
+
+    cy.get('#cad-senha')
+      .type('123456')
+
+  })
+
+  it('Preenche formulário de login', () => {
+
+    cy.visit('http://127.0.0.1:8080/login.html')
+
+    cy.contains('Entrar').click()
+
+    cy.get('#login-email')
+      .type('teste@email.com')
+
+    cy.get('#login-senha')
+      .type('123456')
 
   })
 
