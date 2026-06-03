f4452df715a7522a6a3bdbac69ffc09032266443
Babis2b4 <barbarafalcao099@gmail.com>
2026-06-03 19:35:30 -0300

    correção cypress 2

--- cypress/e2e/fluxos.cy.js
+++ cypress/e2e/fluxos.cy.js
@@ -1,59 +1,40 @@
-// Teste de carregamento da página inicial 
-// Teste de navegação para tela de cadastro
-// Teste de preenchimento do formulário de login e cadastro
-// Teste de verificação do menu principal 
-// Teste de verificação do texto principal
-// Teste de interação com os cards de música
-
 describe('Fluxo do site', () => {
- 
-  // 1. TESTE DE CARREGAMENTO DA PÁGINA
+
   it('Acessa a página inicial', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
   })
 
   it('Verifica se a página carregou corretamente', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
     cy.get('body').should('exist')
   })
 
-  // 2. TESTE DE NAVEGAÇÃO
   it('Acessa aba de cadastro', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
     cy.contains('Cadastrar').click()
   })
 
-  // 3. TESTE DE FORMULÁRIO / LOGIN
   it('Preenche formulário de cadastro', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
     cy.contains('Cadastrar').click()
+    cy.get('#form-cadastro').should('be.visible')
     cy.get('#cad-nome').type('lais')
     cy.get('#cad-email').type('teste@email.com')
     cy.get('#cad-senha').type('123456')
   })
 
-  it('Preenche formulário de login', () => {
-    cy.visit('http://127.0.0.1:8080/')
-    cy.contains('Entrar').click()
-    cy.get('#login-email').type('teste@email.com')
-    cy.get('#login-senha').type('123456')
-  })
-
-  // 4. TESTE DE ELEMENTOS VISÍVEIS
   it('Verifica se o menu existe', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
     cy.get('nav').should('exist')
   })
 
-  // 5. VERIFICAÇÃO DE TEXTO
   it('Verifica se o texto principal aparece', () => {
-    cy.visit('http://127.0.0.1:8080/login.html')
+    cy.visit('http://localhost:3000/login.html')
     cy.contains('Descubra e avalie sua música')
   })
 
-  // 6. TESTE DOS CARDS DE MÚSICA
   it('Clica em um card de música', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://localhost:3000/')
     cy.get('.music-card').first().click()
   })
 
