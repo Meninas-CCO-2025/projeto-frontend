82861a0968e5f4471180e6633f9987210137ae6d
Giovana de Godoy <programacaogiovana@gmail.com>
2026-06-03 15:27:11 -0300

    correção CI

--- cypress/e2e/fluxos.cy.js
+++ cypress/e2e/fluxos.cy.js
@@ -1,99 +1,53 @@
-/* 
-1. Teste de carregamento da página 
-2. Teste de navegação 
-3. Teste de formulário/login
-4. Teste de elementos visíveis
-5. Verificação de texto
-6. Teste dos cards de música
-
-*/
-
 describe('Fluxo do site', () => {
 
   // 1. TESTE DE CARREGAMENTO DA PÁGINA
   it('Acessa a página inicial', () => {
-
-    cy.visit('http://127.0.0.1:8080')
-
+    cy.visit('/')
   })
 
   it('Verifica se a página carregou corretamente', () => {
-
-    cy.visit('http://127.0.0.1:8080')
-
+    cy.visit('/')
     cy.get('body').should('exist')
-
   })
 
   // 2. TESTE DE NAVEGAÇÃO
   it('Acessa aba de cadastro', () => {
-
-    cy.visit('http://127.0.0.1:8080/login.html')
-
+    cy.visit('/login.html')
     cy.contains('Cadastrar').click()
-
   })
 
   // 3. TESTE DE FORMULÁRIO / LOGIN
   it('Preenche formulário de cadastro', () => {
-
-    cy.visit('http://127.0.0.1:8080/login.html')
-
+    cy.visit('/login.html')
     cy.contains('Cadastrar').click()
-
-    cy.get('#cad-nome')
-      .type('lais')
-
-    cy.get('#cad-email')
-      .type('teste@email.com')
-
-    cy.get('#cad-senha')
-      .type('123456')
-
+    cy.get('#cad-nome').type('lais')
+    cy.get('#cad-email').type('teste@email.com')
+    cy.get('#cad-senha').type('123456')
   })
 
   it('Preenche formulário de login', () => {
-
-    cy.visit('http://127.0.0.1:8080/login.html')
-
+    cy.visit('/login.html')
     cy.contains('Entrar').click()
-
-    cy.get('#login-email')
-      .type('teste@email.com')
-
-    cy.get('#login-senha')
-      .type('123456')
-
+    cy.get('#login-email').type('teste@email.com')
+    cy.get('#login-senha').type('123456')
   })
 
-    // 4. TESTE DE ELEMENTOS VISÍVEIS
-
+  // 4. TESTE DE ELEMENTOS VISÍVEIS
   it('Verifica se o menu existe', () => {
-
-    cy.visit('http://127.0.0.1:8080')
-
+    cy.visit('/')
     cy.get('nav').should('exist')
-
   })
 
-   // 5. VERIFICAÇÃO DE TEXTO
+  // 5. VERIFICAÇÃO DE TEXTO
   it('Verifica se o texto principal aparece', () => {
-
-    cy.visit('http://127.0.0.1:8080/login.html')
-
+    cy.visit('/login.html')
     cy.contains('Descubra e avalie sua música')
-
   })
 
-   // 6. TESTE DOS CARDS DE MÚSICA
+  // 6. TESTE DOS CARDS DE MÚSICA
   it('Clica em um card de música', () => {
-
-    cy.visit('http://127.0.0.1:8080')
-
-    cy.get('.music-card')
-      .first()
-      .click()
-
+    cy.visit('/')
+    cy.get('.music-card').first().click()
   })
 
 })
